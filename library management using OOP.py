f9f675f09ba257ed6b1f2f6634a48201489d4707
paarthmodgil <53952680+paarthmodgil@users.noreply.github.com>
2020-07-12 19:21:17 +0530

    Add files via upload

---
+++ library management using OOP.py
@@ -0,0 +1,98 @@
+import datetime
+
+class Library:
+    
+    def __init__(self, name):
+        self.name = name
+        self.books = {} #dictionary
+             
+    
+    def print_all_books(self):
+        for book in self.books.keys():
+            print(book.name, book.ISBN, book.Author)
+            
+
+    
+class Book:
+    def __init__(self, name, ISBN, Author):
+        self.name = name
+        self.ISBN = ISBN
+        self.Author = Author
+        
+    def print_all_details(self):
+        print(self.name, self.ISBN, self.Author)
+        
+
+
+class Student:
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+        #books = {book_name: issue_date}
+        self.books = {}
+
+    def print_all_my_books(self):
+        for book in self.books.keys():
+            print(book.name)
+            
+        
+                
+class Librarian:
+    
+    def __init__(self, name, library):  
+        self.name = name
+        self.library = library
+    
+    def add_book(self, book, copies):
+        #{book_name: num_of_copies}
+        if book in self.library.books.keys():
+            return 'BOOK ALREADY EXISTS'
+        else:
+            self.library.books[book] = copies
+        
+    def issue_book( self, book, student, date):
+        for lb in self.library.books.keys():
+            if lb.name == book.name:
+                self.library.books[lb] -= 1
+                student.books[book] = date            
+            else:
+                return 'BOOK UNAVAILABLE'
+
+
+        
+if __name__ == '__main__':
+    St_Xaviers_Library = Library('St_Xaviers_Library')
+    print(St_Xaviers_Library.print_all_books())
+    
+   
+    
+    Miss_Briganza = Librarian('Miss_Briganza', St_Xaviers_Library)
+
+    
+    book1 = Book('Romeo and Juliet', 'T6JSS','Shakespeare')
+    book2 = Book('Harry Potter', "SAW77", 'J.K. Rowling')
+    book3 = Book("Quantitative Aptitude", "GAM875", "R. S. Aggarwal")
+    book4 = Book("Concepts of Physics", "LSG3723", "H. C. Verma")
+    
+   
+    
+  ### PRINT ALL DETAILS
+    
+    Miss_Briganza.add_book(book1, 10)
+    Miss_Briganza.add_book(book3, 23)
+    Miss_Briganza.add_book(book2, 78)
+    Miss_Briganza.add_book(book4, 67)
+    St_Xaviers_Library.print_all_books()
+    
+    rahul = Student('rahul', 3)
+    anjali = Student("anjali", 1)
+    tina = Student("tina", 1)
+
+    
+    Miss_Briganza.issue_book(book1, rahul, datetime.date(2020,7,1))
+    Miss_Briganza.issue_book(book3, tina, datetime.date(2020,5,3))
+    
+    St_Xaviers_Library.print_all_books()
+    rahul.print_all_my_books()
+    tina.print_all_my_books()
+    anjali.print_all_my_books()
